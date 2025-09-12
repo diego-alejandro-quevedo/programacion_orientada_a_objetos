7067261cdbe56b2199105f8056a87ffc32b8e48b
diegoQ <diegoaquevedo@ucundinamarca.edu.co>
2025-09-12 17:47:45 -0500

    hola

---
+++ src/Persona.java
@@ -0,0 +1,22 @@
+public class Persona{
+
+    public  String nombre;
+    private String cedula;
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setCedula(String nombre){
+        this.cedula=cedula;
+    }
+
+    public String getCedula(){
+        return cedula;
+    }
+
+}
